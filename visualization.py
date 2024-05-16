1853b1d4b71137a941271d9fc7897bfa4f6291f7
Damiam Alfaro <149447958+DamiamAlfaro@users.noreply.github.com>
2024-05-15 19:17:03 -0700

    Add files via upload

---
+++ visualization.py
@@ -0,0 +1,112 @@
+import math
+import pandas as pd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class mathematics():
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    print("Mathematics | Programming | Profession | Finance\n")
+    #functions = {"MATHEMATICS":mathematics,
+     #            "PROGRAMMING":programming,
+      #           "PROFESSION":profession,
+      #           "FINANCE":finance}
+
+    choice = input("Side: ")
+    print(choice.upper())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
