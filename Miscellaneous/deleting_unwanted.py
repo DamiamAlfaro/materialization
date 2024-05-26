6e1307a144ffa1683c0ebc722ac8ae759c9d4ab9
Damiam Alfaro <149447958+DamiamAlfaro@users.noreply.github.com>
2024-05-26 15:07:10 -0700

    Add files via upload

---
+++ Miscellaneous/deleting_unwanted.py
@@ -0,0 +1,17 @@
+import os
+
+def delete_ds_store_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == '.DS_Store':
+                file_path = os.path.join(root, file)
+                try:
+                    os.remove(file_path)
+                    print(f'Deleted: {file_path}')
+                except Exception as e:
+                    print(f'Error deleting {file_path}: {e}')
+
+# Specify the directory to clean
+directory = '/Users/damiamalfaro/Desktop/Materialization/Excel'
+
+delete_ds_store_files(directory)
